7f6dc4ff31271474ae2742d0ed612d0f5ddf3f73
jadenilsoncs <47215341+jadenilsoncs@users.noreply.github.com>
2023-12-26 14:54:14 -0300

    Update app.py
    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ st.set_page_config(layout="wide")
 st.title("Dashboard da Posição Diária :shopping_trolley:")
 
 #criar 4 habas. Chamo o streamlit tabs e passo uma lista, em formato de lista.
-aba1, aba2, aba3, aba4, aba5, aba6 = st.tabs(["Geral","Datas","Coop. Ativos","Emprestimo","Depositos(Total)","Média Cooperados e Media de Emprestimos"])
+aba1, aba2, aba3, aba4, aba5, aba6, aba7 = st.tabs(["Geral","Datas","Coop. Ativos","Emprestimo","Depositos(Total)","Média Cooperados e Media de Emprestimos", 
+    "Tabela da Média dos Cooperados e Media dos Emprestimos"])
 
 #Aqui já mostra uma coluna da planilha por aba (aba1: "Geral(essa motra tudo)", aba2: "Datas", aba3: "Coop. Ativos", aba4: "Emprestimo", aba5: "Depositos(Total)", 
     #"Média Cooperados e Media de Emprestimos").
@@ -51,5 +52,21 @@ with aba6:
         st.metric("Media de Coop. Ativos", format_number( ( df["Coop. Ativos"].sum() ) / df.shape[0] ), "R$" )
 
     #Cálculo da média de empréstimos concedidos em um período de 19 dias, ou seja, a soma total dividida por 19.
+    with coluna3:
+        st.metric("Media de Emprestimo", format_number( ( df["Emprestimo"].sum() ) / df.shape[0] ), "R$" )
+
+with aba7:
+    #Crio um dashboard com 3 colunas
+    coluna1, coluna2, coluna3 = st.columns(3)
+
+    #Cálculo da quantidade registros existentes coma função df.shape[0].
+    with coluna1:
+        total_registros = st.metric("Quantidade de dias contabilizados", df.shape[0], "dias")
+
+    #Tabela com a média de cooperados ativos em um período de 19 dias, ou seja, a soma total de cooperados dividida por 19 dias.
+    with coluna2:
+        st.metric("Media de Coop. Ativos", format_number( ( df["Coop. Ativos"].sum() ) / df.shape[0] ), "R$" )
+
+    #Tabela da média dos empréstimos concedidos em um período de 19 dias, ou seja, a soma total de todos os empréstimos dividida por 19 dias.
     with coluna3:
         st.metric("Media de Emprestimo", format_number( ( df["Emprestimo"].sum() ) / df.shape[0] ), "R$" )
