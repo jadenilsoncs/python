bd219f16c1cb1581fa4b9f11738ba0b85f36aa40
jadenilsoncs <47215341+jadenilsoncs@users.noreply.github.com>
2023-12-27 07:20:03 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,18 +17,30 @@ def format_number(value, prefix = ""):
         #Retorna uma f string. value formatado em 2 casas decimais.
         return f"{prefix} {value:.2f} milhões"
 #####################################################################################################################
+tipo_de_dado1 = df['Datas'].dtype
+
+print(tipo_de_dado1)
+
+tipo_de_dado2 = df['Coop. Ativos'].dtype
+
+print(tipo_de_dado2)
+
+tipo_de_dado3 = df['Emprestimo'].dtype
+
+print(tipo_de_dado3)
+
+df['Datas'] = df['Datas'].astype('int64') # Convertendo coluna datas para inteiro.
+df['Coop. Ativos'] = df['Coop. Ativos'].astype('int64') # Convertendo coluna Coop. Ativos para inteiro.
+df['Emprestimo'] = df['Emprestimo'].astype('int64') # Convertendo coluna Emprestimo para inteiro.
+
 #Agrupando pela data e fazendo um somatório dos cooperados ativos.
 df_tabela_media_cooperados = df.groupby("Datas")[["Coop. Ativos"]].sum()
 #Eliminando registros duplicados e aplicando formatações.
-df['Datas'] = df['Datas'].astype('int64') # Convertendo coluna datas para inteiro.
-df['Coop. Ativos'] = df['Coop. Ativos'].astype('int64') # Convertendo coluna Coop. Ativos para inteiro.
 df_tabela_media_cooperados = df.drop_duplicates(subset="Datas")[["Datas", "Coop. Ativos"]].merge(df_tabela_media_cooperados, left_on="Datas",right_on="Coop. Ativos").sort_values("Datas", ascending= True)
 
 
 df_tabela_media_emprestimos = df.groupby("Datas")[["Emprestimo"]].sum()
 #Eliminando registros duplicados e aplicando formatações.
-df['Datas'] = df['Datas'].astype('int64') # Convertendo coluna datas para inteiro.
-df['Emprestimo'] = df['Emprestimo'].astype('int64') # Convertendo coluna Emprestimo para inteiro.
 df_tabela_media_emprestimos = df.drop_duplicates(subset="Datas")[["Datas", "Emprestimo"]].merge(df_tabela_media_emprestimos, left_on="Datas",right_on="Emprestimo").sort_values("Datas", ascending= True)
 
 print(df_tabela_media_cooperados)
