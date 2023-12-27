b6ab58ccd117406d94e855794c1338f85bae6df8
jadenilsoncs <47215341+jadenilsoncs@users.noreply.github.com>
2023-12-27 07:03:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,28 +21,34 @@ st.set_page_config(layout="wide")
 st.title("Dashboard da Posição Diária :shopping_trolley:")
 
 #criar 4 habas. Chamo o streamlit tabs e passo uma lista, em formato de lista.
-aba1, aba2, aba3, aba4, aba5, aba6, aba7 = st.tabs(["Geral","Datas","Coop. Ativos","Emprestimo",
-    "Depositos(Total)","Média Cooperados e Media de Emprestimos", 
-        "Tabela da Média dos Cooperados e Media dos Emprestimos"])
+aba1, aba2, aba3, aba4, aba5, aba6, aba7 = st.tabs(["1-Geral","2-Datas","3-Coop. Ativos","4-Emprestimo",
+    "5-Depositos(Total)","6-Média Cooperados e Media de Emprestimos", 
+        "7-Tabela do Somatório dos Cooperados por Data e Tabela do Somatório dos Emprestimos por Data"])
 
 #Aqui já mostra uma coluna da planilha por aba (aba1: "Geral(essa motra tudo)", aba2: "Datas", 
     #aba3: "Coop. Ativos", aba4: "Emprestimo", aba5: "Depositos(Total)", 
-        #"Média Cooperados e Media de Emprestimos").
+        #"Média Cooperados e Media de Emprestimos", "Tabela do Somatório dos Cooperados por Data e Tabela do 
+            #Somatório dos Emprestimos por Data"]).
 with aba1:
     st.dataframe(df)
 
+#Mostra somente a coluna Data.
 with aba2:
     st.dataframe(df["Datas"])
 
+#Mostra somente a coluna Coop. Ativos.
 with aba3:
     st.dataframe(df["Coop. Ativos"])
 
+#Mostra somente a coluna Emprestimos.
 with aba4:
     st.dataframe(df["Emprestimo"])
 
+#Mostra somente a coluna Depositos (Total).
 with aba5:
     st.dataframe(df["Depositos (Total)"])
 
+#Mostra 3 colunas.
 with aba6:
     #Crio um dashboard com 3 colunas
     coluna1, coluna2, coluna3 = st.columns(3)
@@ -60,6 +66,7 @@ with aba6:
     with coluna3:
         st.metric("Media de Emprestimo", format_number( ( df["Emprestimo"].sum() ) / df.shape[0] ), "R$" )
 
+#Mostra 3 colunas.
 with aba7:
     #Crio um dashboard com 3 colunas
     coluna1, coluna2, coluna3 = st.columns(3)
@@ -68,12 +75,14 @@ with aba7:
     with coluna1:
         total_registros = st.metric("Quantidade de dias contabilizados", df.shape[0], "dias")
 
-    #Tabela com a média de cooperados ativos em um período de 19 dias, ou seja, a soma total de 
-        #cooperados dividida por 19 dias.
+    #"Tabela do Somatório dos Cooperados por Data. Chamando do arquivo utils.py(onde se encontra a fórmula)
     with coluna2:
-        st.metric("Media de Coop. Ativos", format_number( ( df["Coop. Ativos"].sum() ) / df.shape[0] ), "R$" )
+        print("teste coluna 2")
+        #df_tabela_media_cooperados = df.groupby("Datas")[["Coop. Ativos"]].sum()
+        #st.metric("Somatório ds Coop. Ativos", df_tabela_media_cooperados)
 
-    #Tabela da média dos empréstimos concedidos em um período de 19 dias, ou seja, a soma total de 
-        #todos os empréstimos dividida por 19 dias.
+    #"Tabela do Somatório dos Emprestimos por Data. Chamando do arquivo utils.py(onde se encontra a fórmula)
     with coluna3:
-        st.metric("Media de Emprestimo", format_number( ( df["Emprestimo"].sum() ) / df.shape[0] ), "R$" )
+        print("teste coluna 3")
+        #df_tabela_media_emprestimos = df.groupby("Datas")[["Emprestimos"]].sum()
+        #st.metric("Somatório dos Emprestimos", df_tabela_media_emprestimos)
