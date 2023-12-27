94ede012c9c5a16c62d222fddf42b6c94dfdf8a3
jadenilsoncs <47215341+jadenilsoncs@users.noreply.github.com>
2023-12-27 07:02:45 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+#Importando os dados do meu dataframe.
+from dataset import df
+
 #Função para formatar números
 def format_number(value, prefix = ""):
     #Vai varrer os dados de vazio até mil e verificar o value
@@ -13,5 +16,20 @@ def format_number(value, prefix = ""):
         value /= 1000
         #Retorna uma f string. value formatado em 2 casas decimais.
         return f"{prefix} {value:.2f} milhões"
-    
-##################################################################################
+#####################################################################################################################
+#Agrupando pela data e fazendo um somatório dos cooperados ativos.
+df_tabela_media_cooperados = df.groupby("Datas")[["Coop. Ativos"]].sum()
+#Eliminando registros duplicados e aplicando formatações.
+df['Datas'] = df['Datas'].astype('int64') # Convertendo coluna datas para inteiro.
+df['Coop. Ativos'] = df['Coop. Ativos'].astype('int64') # Convertendo coluna Coop. Ativos para inteiro.
+df_tabela_media_cooperados = df.drop_duplicates(subset="Datas")[["Datas", "Coop. Ativos"]].merge(df_tabela_media_cooperados, left_on="Datas",right_on="Coop. Ativos").sort_values("Datas", ascending= True)
+
+
+df_tabela_media_emprestimos = df.groupby("Datas")[["Emprestimo"]].sum()
+#Eliminando registros duplicados e aplicando formatações.
+df['Datas'] = df['Datas'].astype('int64') # Convertendo coluna datas para inteiro.
+df['Emprestimo'] = df['Emprestimo'].astype('int64') # Convertendo coluna Emprestimo para inteiro.
+df_tabela_media_emprestimos = df.drop_duplicates(subset="Datas")[["Datas", "Emprestimo"]].merge(df_tabela_media_emprestimos, left_on="Datas",right_on="Emprestimo").sort_values("Datas", ascending= True)
+
+print(df_tabela_media_cooperados)
+print(df_tabela_media_emprestimos)
