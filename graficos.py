80570f43844783ddfcf8c5508fb36622b66b475c
jadenilsoncs <47215341+jadenilsoncs@users.noreply.github.com>
2024-01-02 12:58:35 -0300

    Update graficos.py

--- graficos.py
+++ graficos.py
@@ -1,33 +1,35 @@
 #Biblioteca de plotagem
+import pandas as pd
 import plotly.express as px
 #Importa as fórmulas das tabelas para alimentar o gráfico.
 from utils import df_tabela_soma_cooperados
 from utils import df_tabela_soma_emprestimos
+from dataset import df
 
 #Gráfico da soma dos cooperados
-grafico_map_soma_cooperados = px.scatter_geo(
+grafico_map_soma_cooperados = px.scatter_matrix(
     df_tabela_soma_cooperados,
-    custom_data = df_tabela_soma_cooperados,
-    coop_ativos = "Coop. Ativos",
-    scope = "south america",
+    #lat = "Coop. Ativos",
+    #lon= "Coop. Ativos",
+    #scope = "south america",
     size = "Coop. Ativos",
     template = "seaborn",
     #Quando passar o mouse
-    hover_name = "Coop. Ativos",
-    hover_data = {"Datas": False, "Coop. Ativos": False},
+    hover_name ="Coop. Ativos",
+    hover_data = {"Coop. Ativos": False},
     title = "Gráfico da Soma dos Cooperados"
 )
 
 #Gráfico da soma dos empréstimos
-grafico_map_soma_emprestimos = px.scatter_geo(
+grafico_map_soma_emprestimos = px.scatter_matrix(
     df_tabela_soma_emprestimos,
-    custom_data = df_tabela_soma_emprestimos,
-    coop_ativos = "Emprestimo",
-    scope = "south america",
+    #lat = "Emprestimo",
+    #lon= "Emprestimo",
+    #scope = "south america",
     size = "Emprestimo",
     template = "seaborn",
     #Quando passar o mouse
     hover_name = "Emprestimo",
-    hover_data = {"Datas": False, "Emprestimo": False},
+    hover_data = {"Emprestimo": False},
     title = "Gráfico da Soma dos Empréstimos"
 )
