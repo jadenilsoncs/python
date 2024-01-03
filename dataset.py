c69b86fed2f551a65702b061a182759ff8fb3e9b
jadenilsoncs <47215341+jadenilsoncs@users.noreply.github.com>
2024-01-03 20:17:01 -0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,29 +1,13 @@
 import json
-from time import sleep
 import pandas as pd
 
-# Importar os dados da planilha Excel
-#df = pd.read_excel("/home/jadenilsonsilva/Documents/python/UAD_Posicao_Diaria.xlsx")
-df = pd.read_excel("/home/jadenilsoncs/python/UAD_Posicao_Diaria.xlsx")
-
-# Salvar os dados em formato JSON(transformar em json)
-#df.to_json("/home/jadenilsonsilva/Documents/python/UAD_Posicao_Diaria.json")
-df.to_json("/home/jadenilsoncs/python/UAD_Posicao_Diaria.json")
-
-#Caminho onde estão os dados, agora em formato json. Pode ser um banco de dados também. Estou abrindo o arquivo.
-#file = open("/home/jadenilsonsilva/Documents/python/UAD_Posicao_Diaria.json")
-file = open("/home/jadenilsoncs/python/UAD_Posicao_Diaria.json")
-
-#Carregamento do arquivo json
+file = open('dados/vendas.json')
 data = json.load(file)
 
-#Estrutura python que arremete muito ao json é a estrutura de diciónario. Estou criando um DataFrame baseado em dicionário e passo os dados (data)
-# df é referência a DataFrame
+#print(data)
+
 df = pd.DataFrame.from_dict(data)
 
-#imprime os dados carregados para ter certeza de que os estou acessando. E imprime agora já em formato tabular.
-#Formatando a data no formato dia/mês/ano.
-df["Datas"] = pd.to_datetime(df["Datas"], format="%d/%m/%Y")
+#print(df)
 
-#Fecha o arquivo, o acesso ao arquivo carregado.
-file.close()
+df['Data da Compra'] = pd.to_datetime(df['Data da Compra'], format='%d/%m/%Y')
